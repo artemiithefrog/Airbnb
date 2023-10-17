297a88170ab8f10dabc617a014ee6811e153768f
Artemii <artemiithefrog95@icloud.com>
2023-10-17 16:24:03 +0200

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by artemiithefrog . on 17.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
